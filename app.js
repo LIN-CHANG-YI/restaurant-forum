0b0aaf6b4f6c8f20b65319d3ca1234bafef27fab
LIN <yi19930716@gmail.com>
2020-09-06 11:37:50 +0800

    feat: add restaurant index page,admin restaurant index page,Initialize sequelize and connect to database,Create user model,userController and signup page

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const db = require('./models')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
+
+require('./routes')(app)
