1597c70ddf8c8abd37838b14a7ce445079a2bb2f
LIN <yi19930716@gmail.com>
2020-09-07 21:03:03 +0800

    feat: Listen to process.env.PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const flash = require('connect-flash')
 const passport = require('./config/passport.js')
 const methodOverride = require('method-override')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(methodOverride('_method'))
