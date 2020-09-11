ebec3cd9aba5efccf23ec57437f854c35767d86a
LIN <yi19930716@gmail.com>
2020-09-11 08:49:57 +0800

    feat: Show category name on admin/restaurants and Show category name on admin/restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 const User = db.User
+const Category = db.Category
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 const Handlebars = require('handlebars')
@@ -12,7 +13,7 @@ Handlebars.registerHelper('equal', (item1, item2, options) => {
 
 const adminController = {
   getRestaurants: (req, res) => {
-    return Restaurant.findAll({ raw: true, nest: true })
+    return Restaurant.findAll({ raw: true, nest: true, include: [Category] })
       .then(restaurants => {
         return res.render('admin/restaurants', { restaurants })
       })
@@ -60,7 +61,7 @@ const adminController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true })
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
       .then(restaurant => {
         return res.render('admin/restaurant', { restaurant })
       })
