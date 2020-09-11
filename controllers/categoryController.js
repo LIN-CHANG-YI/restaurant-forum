4d9555d4e795aec4112f5ab74838b8a96532de50
LIN <yi19930716@gmail.com>
2020-09-11 10:55:42 +0800

    feat: Add admin categories edit and put feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,7 +5,14 @@ const categoryController = {
   getCategories: (req, res) => {
     return Category.findAll({ raw: true, nest: true })
       .then(categories => {
-        return res.render('admin/categories', { categories })
+        if (req.params.id) {
+          Category.findByPk(req.params.id, { raw: true, nest: true })
+            .then(category => {
+              res.render('admin/categories', { category, categories })
+            })
+        } else {
+          return res.render('admin/categories', { categories })
+        }
       })
   },
 
@@ -19,6 +26,21 @@ const categoryController = {
       }).then((category) => {
         res.redirect('/admin/categories')
       })
+    }
+  },
+
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    } else {
+      return Category.findByPk(req.params.id)
+        .then(category => {
+          category.update(req.body)
+            .then(category => {
+              res.redirect('/admin/categories')
+            })
+        })
     }
   }
 }
