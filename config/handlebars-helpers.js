7225595d202e7e21c3731e3de8325ae8f104486b
LIN <yi19930716@gmail.com>
2020-09-12 10:03:23 +0800

    feat: Show related comments on restaurant page

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,5 +1,11 @@
+const moment = require('moment')
+
 module.exports = {
   ifCond: (item1, item2, options) => {
     return (item1 === item2) ? options.fn(this) : options.inverse(this)
+  },
+
+  moment: a => {
+    return moment(a).fromNow()
   }
 }
--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 const pageLimit = 10
 
 const restController = {
@@ -35,9 +37,9 @@ const restController = {
   },
 
   getRestaurant: (req, res) => {
-    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: Category })
+    return Restaurant.findByPk(req.params.id, { include: [Category, { model: Comment, include: [User] }] })
       .then(restaurant => {
-        return res.render('restaurant', { restaurant })
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
   }
 }
