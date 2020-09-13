c47b9e097c878e4e7b8a8e50fb1ac3a90f8440f9
LIN <yi19930716@gmail.com>
2020-09-13 15:50:54 +0800

    feat: Add addFavorite and removeFavorite feature

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,7 @@ const db = require('../models')
 const User = db.User
 const Comment = db.Comment
 const Restaurant = db.Restaurant
+const Favorite = db.Favorite
 const fs = require('fs')
 const imgur = require('imgur-node-api')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
@@ -104,6 +105,22 @@ const userController = {
     }
   },
 
+  addFavorite: (req, res) => {
+    return Favorite.create({ UserId: req.user.id, RestaurantId: req.params.restaurantId })
+      .then(restaurant => {
+        return res.redirect('back')
+      })
+  },
+
+  removeFavorite: (req, res) => {
+    return Favorite.findOne({ where: { UserId: req.user.id, RestaurantId: req.params.restaurantId } })
+      .then(restaurant => {
+        restaurant.destroy()
+          .then(restaurant => {
+            res.redirect('back')
+          })
+      })
+  }
 }
 
 module.exports = userController
