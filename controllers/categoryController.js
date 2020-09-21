7b48321fed8e0523075ac92129fb2445c7c01486
LIN <yi19930716@gmail.com>
2020-09-21 14:56:53 +0800

    refactor: refactor categoryController.putCategory

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -21,18 +21,14 @@ const categoryController = {
   },
 
   putCategory: (req, res) => {
-    if (!req.body.name) {
-      req.flash('error_messages', "name didn't exist")
-      return res.redirect('back')
-    } else {
-      return Category.findByPk(req.params.id)
-        .then(category => {
-          category.update(req.body)
-            .then(category => {
-              res.redirect('/admin/categories')
-            }).catch(error => res.sendStatus(404))
-        }).catch(error => res.sendStatus(404))
-    }
+    categoryService.putCategory(req, res, (data) => {
+      if (data.status === 'error') {
+        req.flash('error_messages', data.message)
+        return res.redirect('back')
+      }
+      req.flash('success_messages', data.message)
+      return res.redirect('/admin/categories')
+    })
   },
 
   deleteCategory: (req, res) => {
--- services/categoryService.js
+++ services/categoryService.js
@@ -27,6 +27,21 @@ const categoryService = {
       }).catch(error => res.sendStatus(404))
     }
   },
+
+  putCategory: (req, res, callback) => {
+    if (!req.body.name) {
+      callback({ status: 'error', message: "name didn't exist" })
+    } else {
+      return Category.findByPk(req.params.id)
+        .then(category => {
+          category.update(req.body)
+            .then(category => {
+              callback({ status: 'success', message: "category was successfully updated" })
+              res.redirect('/admin/categories')
+            }).catch(error => res.sendStatus(404))
+        }).catch(error => res.sendStatus(404))
+    }
+  },
 }
 
 module.exports = categoryService
